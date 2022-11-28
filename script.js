5d7c17e9cfeff8a61dad166155083bfc150f4179
Zhixin Cai <zhixin.cai@thetopai.com>
2022-11-28 16:48:46 -0500

    add javascript to our team and remove repeating content

--- script.js
+++ script.js
@@ -172,3 +172,147 @@ document.addEventListener("readystatechange",function(event){
     }
 })
 
+/* Our Team js */
+
+var ourTeamSectionTwo = [
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team11.jpg",
+        "name": "William Dean",
+        "position": "Chief Executive Officer / CEO",
+        "describe": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa. Cum sociis numquasa mode tempora posuere feugiat.",
+        "workPhone": "1-800-123-4567 - Extension 114",
+        "telephone": "1-902-361-7714",
+        "email": "william@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team21.jpg",
+        "name": "Leah Jennings",
+        "position": "Chief Financial Officer / CFO",
+        "describe": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa. Cum sociis numquasa mode tempora posuere feugiat.",
+        "workPhone": "1-800-123-4567 - Extension 107",
+        "telephone": "1-902-342-0864",
+        "email": "leah@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team31.jpg",
+        "name": "Zachary Hale",
+        "position": "Lead Sales Manager",
+        "describe": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa. Cum sociis numquasa mode tempora posuere feugiat.",
+        "workPhone": "1-800-123-4567 - Extension 119",
+        "telephone": "1-902-832-3702",
+        "email": "zachary@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+]
+function OurTeam_threeImage(){
+    let outHtml = "";
+    for(let i = 0; i < ourTeamSectionTwo.length; i++){
+        outHtml += '<div class="column col-sm-12 col-md-12 col-lg-4 manager-infor">';
+        outHtml += '<div class="manager-img">';
+        outHtml += '<img src="'+ourTeamSectionTwo[i].imageUrl+'" alt="William Dean" width="300" height="200">';
+        outHtml += '<div>';
+        outHtml += '<h4><strong>'+ourTeamSectionTwo[i].name+'</strong></h4>';
+        outHtml += '<p>'+ourTeamSectionTwo[i].position+'</p>';
+        outHtml += '</div>';
+        outHtml += '<hr>';  
+        outHtml += '<div>'; 
+        outHtml += '<p>'+ourTeamSectionTwo[i].describe+'</p>'; 
+        outHtml += '<ul>'; 
+        outHtml += '<li><span>'+ourTeamSectionTwo[i].workPhone+'</span></li>'; 
+        outHtml += '<li><span>'+ourTeamSectionTwo[i].telephone+'</span></li>'; 
+        outHtml += '<li><a class="email" href="">'+ourTeamSectionTwo[i].email+'</a></li>';
+        outHtml += '</ul>';
+        outHtml += '</div>'; 
+        outHtml += '<hr>';
+        outHtml += '<div>'; 
+        outHtml += '<img src="'+ourTeamSectionTwo[i].socialMedia+'" width="250px">';
+        outHtml += '</div>'; 
+        outHtml += '</div>'; 
+        outHtml += '</div>'; 
+    }
+    return outHtml;
+}
+
+var ourTeamSectionThree = [
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team4-lrg1.jpg",
+        "name": "Luca Sanderson",
+        "position": "Sales Representative",
+        "describe": "Lorem ipsum dolor sit amet, paleotousia consectetuer adipiscing elit. Aenean com.",
+        "workPhone": "1-800-123-4567 - Extension 105",
+        "telephone": "1-902-544-4415",
+        "email": "luca@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team5-lrg1.jpg",
+        "name": "Abby Myers",
+        "position": "Sales Representative",
+        "describe": "Lorem ipsum dolor sit amet, paleotousia consectetuer adipiscing elit. Aenean com.",
+        "workPhone": "1-800-123-4567 - Extension 123",
+        "telephone": "1-902-361-7267",
+        "email": "abby@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team6-lrg1.jpg",
+        "name": "Connor Wyatt",
+        "position": "Sales Representative",
+        "describe": "Lorem ipsum dolor sit amet, paleotousia consectetuer adipiscing elit. Aenean com.",
+        "workPhone": "1-800-123-4567 - Extension 111",
+        "telephone": "1-902-544-4415",
+        "email": "connor@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+    {
+        "imageUrl": "https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/team7-lrg1.jpg",
+        "name": "Sarah Thomas",
+        "position": "Sales Representative",
+        "describe": "Lorem ipsum dolor sit amet, paleotousia consectetuer adipiscing elit. Aenean com.",
+        "workPhone": "1-800-123-4567 - Extension 108",
+        "telephone": "1-902-544-4415",
+        "email": "sarah@automotivetemplate.com",
+        "socialMedia":"icon.jpg",
+    },
+]
+function OurTeam_fourImage(){
+    let outHtml = "";
+    for(let i = 0; i < ourTeamSectionTwo.length; i++){
+        outHtml += '<div class="column col-sm-12 col-md-6 col-lg-3 col-xl-3 sales-infor">';
+        outHtml += '<div class="sales-infor-img">';
+        outHtml += '<img src="'+ourTeamSectionThree[i].imageUrl+'" alt="William Dean" height="200">';
+        outHtml += '<div>';
+        outHtml += '<h4><strong>'+ourTeamSectionThree[i].name+'</strong></h4>';
+        outHtml += '<p>'+ourTeamSectionThree[i].position+'</p>';
+        outHtml += '</div>';
+        outHtml += '<hr>';  
+        outHtml += '<div>'; 
+        outHtml += '<p>'+ourTeamSectionThree[i].describe+'</p>'; 
+        outHtml += '<ul>'; 
+        outHtml += '<li><span>'+ourTeamSectionThree[i].workPhone+'</span></li>'; 
+        outHtml += '<li><span>'+ourTeamSectionThree[i].telephone+'</span></li>'; 
+        outHtml += '<li><a class="email" href="">'+ourTeamSectionThree[i].email+'</a></li>';
+        outHtml += '</ul>';
+        outHtml += '</div>'; 
+        outHtml += '<hr>';
+        outHtml += '<div>'; 
+        outHtml += '<img src="'+ourTeamSectionThree[i].socialMedia+'" width="200px">';
+        outHtml += '</div>'; 
+        outHtml += '</div>'; 
+        outHtml += '</div>'; 
+    }
+    return outHtml;
+}
+
+document.addEventListener("readystatechange",function(event){
+    if(event.target.readyState == "interactive"){
+        if(document.getElementById("OurTeam_threeImage")!= null){
+            document.getElementById("OurTeam_threeImage").innerHTML = OurTeam_threeImage();
+        }
+        if(document.getElementById("OurTeam_fourImage")!= null){
+            document.getElementById("OurTeam_fourImage").innerHTML = OurTeam_fourImage();
+        }
+    }
+})
